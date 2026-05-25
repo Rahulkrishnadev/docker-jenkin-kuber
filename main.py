81bca04f43292b9af2b1298ed2d255218c29774b
Rahul <reboundrahul777@gmail.com>
2026-05-25 15:46:58 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class evennumber(BaseModel):
+    num:int
+
+
+    
+def even(num):
+    if num % 2 == 0:
+       return "it is an even number"
+    else:
+        return "it is not an even number"
+
+# number=int(input("enter a number:"))
+# result=even(number)
+# print(result)
+@app.post("/check")
+async def final(data:evennumber):
+    result=even(data.num)
+    return result
